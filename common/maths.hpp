a6c79465fd42247b31e8ab435479fc0934d1ce85
Winsense2 <22506181@stu.mmu.ac.uk>
2025-05-18 13:59:15 +0100

    1st commit

--- common/maths.hpp
+++ common/maths.hpp
@@ -1,1 +1,25 @@
 
+// Maths.h
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+class Maths {
+public:
+    static glm::mat4 translate(const glm::vec3& translation) {
+        return glm::translate(glm::mat4(1.0f), translation);
+    }
+
+    static glm::mat4 scale(const glm::vec3& scaling) {
+        return glm::scale(glm::mat4(1.0f), scaling);
+    }
+
+    static glm::mat4 rotate(float angle, const glm::vec3& axis) {
+        return glm::rotate(glm::mat4(1.0f), angle, axis);
+    }
+
+    static float radians(float degrees) {
+        return glm::radians(degrees);
+    }
+};
